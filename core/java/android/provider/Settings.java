23e6e425eeaa43d6ca9b50e3fb76e302ec1c0b5f
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    Port: Wake on plug (1/2)
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3684,6 +3684,11 @@ public final class Settings {
         public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
 
         /**
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -9112,6 +9117,7 @@ public final class Settings {
             CALL_AUTO_RETRY,
             DOCK_AUDIO_MEDIA_ENABLED,
             ENCODED_SURROUND_OUTPUT,
+            WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
             LOW_POWER_MODE_TRIGGER_LEVEL
         };
 
