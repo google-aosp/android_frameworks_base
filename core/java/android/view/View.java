450f4bfa3a6f9efea08784061a44acd27f4bcda0
ishantvivek <ishantvivek1311@gmail.com>
2016-09-17 11:22:26 +0000

    Revert "Revert "Expose setDrawGLFunctionDetachedCallback to webview""
    This reverts commit d443f36957a2f8a6aa295bd88a5bf7886d56a064.

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -3906,6 +3906,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
      * cleanup.
      */
     final RenderNode mRenderNode;
+    private Runnable mRenderNodeDetachedCallback;
 
     /**
      * Set to true when the view is sending hover accessibility events because it
@@ -16075,6 +16076,20 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
      * @hide
      */
     public void onRenderNodeDetached(RenderNode renderNode) {
+        if (renderNode == mRenderNode && mRenderNodeDetachedCallback != null) {
+            mRenderNodeDetachedCallback.run();
+        }
+    }
+
+    /**
+     * Set callback for functor detach. Exposed to WebView through WebViewDelegate.
+     * Should not be used otherwise.
+     * @hide
+     */
+    public final Runnable setRenderNodeDetachedCallback(@Nullable Runnable callback) {
+        Runnable oldCallback = mRenderNodeDetachedCallback;
+        mRenderNodeDetachedCallback = callback;
+        return oldCallback;
     }
 
     /**
